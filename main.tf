767751e24f2425e53831f1b700b9471462b52e5f
kolicivan <ivan.kolic@naovis.com>
2025-05-18 13:01:18 +0200

    HS-1 - Deploy storage account

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "terraform-storage-accounts" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "terraform-storage-accounts" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.terraform-storage-accounts.name
+  location                 = azurerm_resource_group.terraform-storage-accounts.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "terraform-storage-accounts" {
+  name                  = "terraform-storage-accounts"
+  storage_account_name  = azurerm_storage_account.terraform-storage-accounts.name
+  container_access_type = "private"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable resource_group_name {
+    default = "tfhylasticx"
+}
+
+variable storage_account_name {
+    default = "tfhylastix"
+}
+
+variable location {
+    default = "westeurope"
+}
